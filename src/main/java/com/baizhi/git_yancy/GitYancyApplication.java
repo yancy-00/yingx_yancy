8486531a6bfc88fcc43f13c43c576967960302ed
yancy <56312140@qq.com>
2020-11-30 22:45:02 +0800

    项目中第一次提交

---
+++ src/main/java/com/baizhi/git_yancy/GitYancyApplication.java
@@ -0,0 +1,13 @@
+package com.baizhi.git_yancy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitYancyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitYancyApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/baizhi/git_yancy/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.baizhi.git_yancy;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(GitYancyApplication.class);
+    }
+
+}
